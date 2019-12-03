8fdd7ec47f803a70abfc4be3ea355cec232bf8b5
Isha Karki <52482575+ishakarki@users.noreply.github.com>
2019-12-03 15:19:37 -0700

    Update Pokemon.hpp

--- Pokemon.hpp
+++ Pokemon.hpp
@@ -16,7 +16,7 @@ struct pokeNode
 
     pokeNode(){}//default constructor
 
-    pokeNode(string n, float w, string t1, string t2, string d) : name(n), weight(w), 
+    pokeNode(int i, string n, float w, string t1, string t2, string d) : index(i), name(n), weight(w), 
     type1(t1), type2(t2), desc(d){} //parametized constructor 
 
 }; 
@@ -25,16 +25,22 @@ class PokeHash
 {
     public: 
         PokeHash(); //constructor
-        bool insertPokemon(string name); //when true, this will output to user pokemon file
-        void printByType(string type); 
-        pokeNode *searchPokemon(string name);
+        void printTypes(); 
+        bool insertPokemon(string name, string userFile); //when true, this will output to user pokemon file
+        bool printByType(int type); // prints the type and all pokemon under that type
+        void printPokemonByTypes(); //prints specific pokemon under one type 
+        pokeNode *searchPokemon(string name);//searches for pokemon by using the hash table
+        void sort(pokeNode *p); //sorts pokemon into their respective type vectors
         void readInPokemonFile(); //reads in original pokemon file
-        void readInUserFile(); //reads in users file
-        unsigned int hashFunction(string key); 
+        void readInUserFile(string userFile); //reads in users file
+        unsigned int hashFunction(string key); //creates the index for each pokemon
+        void printPokemonFromVector(int index, vector<pokeNode*>type); //prints from vector
+        void printPokemonFromHash(string name); //prints pokemon by looking at a hash index
+        //void access();
 
     private: 
-        const int tableSize = 1049;//number of pokemon that currently exist * 1.3
-        pokeNode* *table[1049]; 
+        int tableSize = 1049;//number of pokemon that currently exist * 1.3
+        pokeNode* table[1049]; //hashtable
         vector<pokeNode*> normal;
         vector<pokeNode*> fire;
         vector<pokeNode*> water;
@@ -53,10 +59,8 @@ class PokeHash
         vector<pokeNode*> dragon;
         vector<pokeNode*> steel;
         vector<pokeNode*> fairy;
+
+        pokeNode* createPokemon(int i, string n, float w, string t1, string t2, string d, pokeNode* next);//creates the pokemon node
     
-        pokeNode *createNode(string name, pokeNode* next); 
-
-        
-
 };
 #endif
